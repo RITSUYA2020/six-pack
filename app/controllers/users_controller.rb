f98b3efd2bb952fae04632d0fbd2e804d1bf5747
RITSUYA2020 <12ritsuya23@gmail.com>
2020-07-20 08:47:48 +0000

    [Add]投稿全般に関する機能、ユーザー情報編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,31 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @work_outs = @user.work_outs
   end
 
-  def edit; end
+  def edit
+  	@user = User.find(params[:id])
+  end
 
-  def update; end
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(current_user)
+  	else
+  		flash[:error] = '名前とメールアドレスを入力してください。'
+  		render "edit"
+  	end
+  end
 
-  def confirm; end
+  def confirm
+  end
 
-  def withdraw; end
+  def withdraw
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email)
+  end
 end
--- app/controllers/work_outs_controller.rb
+++ app/controllers/work_outs_controller.rb
@@ -2,22 +2,51 @@ class WorkOutsController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @work_outs = WorkOut.all
+    @work_outs = WorkOut.all.reverse_order
   end
 
   def show
     @work_out = WorkOut.find(params[:id])
+    @user = @work_out.user
   end
 
-  def edit; end
+  def edit
+    @work_out = WorkOut.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @work_out = WorkOut.find(params[:id])
+    if @work_out.update(work_out_params)
+      redirect_to work_out_path(current_user)
+    else
+      flash[:error] = '*の項目を入力してください。'
+      render "edit"
+    end
+  end
 
-  def destroy; end
+  def destroy
+    work_out = WorkOut.find(params[:id])
+    work_out.destroy
+    redirect_to work_outs_path
+  end
 
   def new
     @work_out = WorkOut.new
   end
 
-  def create; end
+  def create
+    @work_out = WorkOut.new(work_out_params)
+    @work_out.user_id = current_user.id
+    if @work_out.save
+      redirect_to work_out_path(current_user)
+    else
+      flash[:error] = '*の項目を入力してください。'
+      render "new"
+    end
+  end
+
+  private
+  def work_out_params
+    params.require(:work_out).permit(:time, :muscle_group, :muscle_group, :body, :effect, :place, :before_image, :after_image)
+  end
 end
