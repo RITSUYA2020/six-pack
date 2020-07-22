44f751465a8d4fd02d171f2b7ea6ef4df52abe06
RITSUYA2020 <12ritsuya23@gmail.com>
2020-07-22 04:20:28 +0000

    [Add]フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,16 @@ class UsersController < ApplicationController
   def withdraw
   end
 
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :email)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/:user_id/favorites' => 'favorites#show', as: 'user_favorites'
-  resources :users, except: [:index, :create, :new]
+
+  resources :users, except: [:index, :create, :new] do
+  	resource :relationships, only: [:create, :destroy]
+  	get :follows
+  	get :followers
+  end
+
   resources :work_outs do
   	resource :favorites, only: [:create, :destroy]
   	resource :comments, only: [:create, :destroy]
