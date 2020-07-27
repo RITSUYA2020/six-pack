8a27d692a0a5156e02443631eddd5270d000e63a
vagrant <vagrant@localhost.localdomain>
2020-07-27 12:40:02 +0000

    [Update]migrationファイル

--- db/migrate/20200727113845_change_column_to_allow_null.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToAllowNull < ActiveRecord::Migration[5.2]
-  def up
-  	change_column :work_outs, :body,:text, null: true
-  end
-
-  def down
-  	change_column :work_outs, :body,:text, null: false
-  end
-end
--- db/migrate/20200727115023_change_body_default_on_comments.rb
+++
@@ -1,9 +0,0 @@
-class ChangeBodyDefaultOnComments < ActiveRecord::Migration[5.2]
-  def up
-  	change_column :comments, :body,:text, null: true
-  end
-
-  def down
-  	change_column :comments, :body,:text, null: false
-  end
-end
