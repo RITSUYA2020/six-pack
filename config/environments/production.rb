f79eaef124dc399fc6e23aa5a8a4479c61ad199e
vagrant <vagrant@localhost.localdomain>
2020-08-01 08:54:39 +0000

    [Fix]本番環境での問い合わせメール送受信

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = {  :host => 'http://54.199.39.212' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  user_name = ENV['SEND_MAIL']
+  password = ENV['SEND_MAIL_PASSWORD']
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:             user_name,
+    password:              password,
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+
 end
